339e220de629f9f6460f32284159dafcb8da1592
sraime <robin.collas@gmial.com>
2020-08-30 17:34:03 +0100

    config

---
+++ config/config.template.ts
@@ -0,0 +1,8 @@
+export let token: string = "";
+export let prefix: string = "";
+export let owners: string[] = [];
+export let db = {
+  host: "localhost",
+  port: 27017,
+  name: "overscrim-agent"
+};
